90a4bc6c59ffb5e17c5785d237a42bd3693fba47
mangtrungzos <sangv906@gmail.com>
2025-03-20 23:29:53 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "../providers/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Goat notes",
@@ -22,11 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster />
+          </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+function Header() {
+    return ( 
+        <header>
+            <Link href="/">
+                <Image 
+                    src="/public/goatius.png"
+                    height={60}
+                    width={60}
+                    alt="logo"
+                    className="rounded-full"
+                    priority
+                />
+            </Link>
+        </header>
+     );
+}
+
+export default Header;
